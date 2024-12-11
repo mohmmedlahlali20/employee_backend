fba35069f38ea704ffafd62a557919b3368c12e9
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-11 23:34:07 +0100

    feat: add source files

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EmployeeModule } from './employee/employee.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:2717/employee'),
+    EmployeeModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
